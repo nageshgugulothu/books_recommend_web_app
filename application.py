a65317b99c5652df48e537ef4b6110c94e07ff83
laxmisharvan <nageshgugulothu@gmail.com>
2024-02-23 21:35:11 +0530

    updated code

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def recommend():
     return render_template('result.html', book_title=book_title, recommended_books=recommended_books)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
